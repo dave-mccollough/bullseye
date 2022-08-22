01101d4b7d5a64ad28bbf3503a3c72759cc77284
Dave McCollough <davemccollough@gmail.com>
2022-08-21 20:19:02 -0500

    Initial Commit

---
+++ bullseye/bullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullseyeApp.swift
+//  bullseye
+//
+//  Created by Dave McCollough on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct bullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
