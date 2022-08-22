32474c79df4d7402f9837240dc56223cdca6f151
Dave McCollough <davemccollough@gmail.com>
2022-08-21 22:32:37 -0500

    Added styling and initial popup.

--- bullseye/ContentView.swift
+++ bullseye/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    @State allows SwiftUI to manage state
+    @State private var alertIsVisible: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯 \nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .kerning(2.0)
+                .bold()
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack {
+                Text("1")
+                .bold()
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+                .bold()
+            }
+            Button(action: {
+                print("Hello SwiftUI!")
+                self.alertIsVisible = true
+            }){
+                Text("Hit Me!")
+            }
+            .alert("Hello There!", isPresented: $alertIsVisible) {
+                Button("Awesome!") {}
+            } message: {
+                Text("This is my first popup!")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
